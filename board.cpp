855ff56b16d55862015b55106517bd4afc27df6f
SeanCrosby <seannedcrosby@gmail.com>
2026-09-09 13:23:10 -0500

    Generate bombs function implemented

--- board.cpp
+++ board.cpp
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <ctime>
 #include <iostream>
 #include "tile.h"
 using namespace std;
@@ -31,8 +32,34 @@ tile** generateBlankBoard() {
     return board;
 }
 
+tile** generateBombs(tile** board, int bombs, int initialX, int initialY) {
+    int bombCount = bombs;
+    tile** bombBoard = board;
+    int minSafeX = initialX == 0 ? 0 : initialX - 1;
+    int maxSafeX = initialX == 9 ? 9 : initialX + 1;
+    int minSafeY = initialY == 0 ? 0 : initialY - 1;
+    int maxSafeY = initialY == 9 ? 9 : initialY + 1;
+
+    while(bombCount > 0){
+        int canaditeY = rand() % BOARD_SIZE;
+        int canaditeX = rand() % BOARD_SIZE;
+
+        tile canaditeTile = board[canaditeY][canaditeX];
+        if (canaditeTile.tileValue != 9 && 
+            (canaditeX < minSafeX || canaditeX > maxSafeX || 
+            canaditeY < minSafeY || canaditeY > maxSafeY)) {
+                bombBoard[canaditeY][canaditeX].tileValue = 9;
+                bombCount -= 1;
+        }
+    }
+
+    return bombBoard;
+}
+
 int main() {
+    srand(time(nullptr)); //Seed random so board is actually random
     tile** board = generateBlankBoard();
-    printBoard(board);
+    tile** bombBoard = generateBombs(board, 10, 9, 9);
+    printBoard(bombBoard);
     return 0;
 }
