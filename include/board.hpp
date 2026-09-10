7ac91395e78cc2433bdaeff034661265061edf46
SeanCrosby <seannedcrosby@gmail.com>
2026-09-10 13:33:53 -0500

    Started on moving board generation to Constructor, have not tested new code yet

--- include/board.hpp
+++ include/board.hpp
@@ -1,5 +1,8 @@
 //for std::size_t
 #include <cstddef>
+#include <stdlib.h>
+
+const int BOARD_SIZE = 10;
 
 enum TileState {
     FLAGGED,
@@ -9,16 +12,72 @@ enum TileState {
 
 class Tile {
     public:
-        int tileValue; //0 for blank, 9 for bomb
-        TileState tileState;
+        int tile_value; //0 for blank, 9 for bomb
+        TileState tile_state;
 };
 
 class Board {
 	private:
 		Tile** tiles;
 	public:
-		//Move generate board code to this function
-		Board(std::size_t board_size);
+		// Board Constructor
+		Board(std::size_t board_size, int bombs, int initial_y, int initial_x) {
+
+			// Generate a blank board
+			Tile** board_tiles = new Tile*[BOARD_SIZE];
+			for (int i = 0; i < BOARD_SIZE; ++i) {
+				board_tiles[i] = new Tile[BOARD_SIZE];
+				for (int j = 0; j < BOARD_SIZE; ++j) {
+					Tile new_tile;
+					new_tile.tile_value = 0;
+					new_tile.tile_state = COVERED;
+					board_tiles[i][j] = new_tile;
+				}
+			}
+
+			//Fill board with bombs and numbers
+			int bombCount = bombs;
+			Tile** game_board_tiles = board_tiles;
+			int min_safe_x = initial_x == 0 ? 0 : initial_x - 1;
+			int max_safe_x = initial_x == 9 ? 9 : initial_x + 1;
+			int min_safe_y = initial_y == 0 ? 0 : initial_y - 1;
+			int max_safe_y = initial_y == 9 ? 9 : initial_y + 1;
+
+			while(bombCount > 0){
+				int canaditeY = rand() % BOARD_SIZE;
+				int canaditeX = rand() % BOARD_SIZE;
+
+				Tile canaditeTile = game_board_tiles[canaditeY][canaditeX];
+				if (canaditeTile.tile_value != 9 && 
+					(canaditeX < min_safe_x || canaditeX > max_safe_x || 
+					canaditeY < min_safe_y || canaditeY > max_safe_y)) {
+						mark_tiles(game_board_tiles, canaditeX, canaditeY);
+						bombCount -= 1;
+				}
+			}
+
+			tiles = board_tiles;
+
+			//
+		};
+
+		void mark_tiles(Tile** board_tiles, int bombX, int bombY) {
+			Tile& bomb_tile = board_tiles[bombY][bombX];
+			bomb_tile.tile_value = 9;
+
+			for(int i = -1; i <= 1; i++) {
+				for(int j = -1; j <= 1; j++) {
+					if((bombX + i >= 0) && (bombX + i < BOARD_SIZE)
+					&& (bombY + j >= 0) && (bombX + j < BOARD_SIZE)) {
+						Tile& update_tile = board_tiles[bombY + j][bombX + i];
+						if(update_tile.tile_value != 9) {
+							update_tile.tile_value += 1;
+						}
+					}
+				}
+			}
+		}
+
 		//Not sure if neccessary but might need to free Board.tiles 
 		~Board();
 };
--- src/board.cpp
+++ src/board.cpp
@@ -1,3 +1,4 @@
+/*
 #include <stdlib.h>
 #include <ctime>
 #include <iostream>
@@ -6,10 +7,10 @@ using namespace std;
 
 const int BOARD_SIZE = 10;
 
-void printBoard(Tile** board) {
+void print_board(Board board) {
     for (int i = 0; i < BOARD_SIZE; ++i) {
         for (int j = 0; j < BOARD_SIZE; ++j) {
-            int tileValue = board[i][j].tileValue;
+            int tileValue = board[i][j].tile_value;
             cout << tileValue<<" ";
         }
         cout << endl;
@@ -18,40 +19,27 @@ void printBoard(Tile** board) {
     return;
 }
 
-Tile** generateBlankBoard() {
-    Tile** board = new Tile*[BOARD_SIZE];
-    for (int i = 0; i < BOARD_SIZE; ++i) {
-        board[i] = new Tile[BOARD_SIZE];
-        for (int j = 0; j < BOARD_SIZE; ++j) {
-            Tile newTile;
-            newTile.tileValue = 0;
-            newTile.tileState = COVERED;
-            board[i][j] = newTile;
-        }
-    }
-    return board;
-}
 
-void markTiles(Tile** board, int bombX, int bombY) {
-    Tile& bombTile = board[bombY][bombX];
-    bombTile.tileValue = 9;
+void mark_tiles(Board board, int bombX, int bombY) {
+    Tile& bomb_tile = board.tiles[bombY][bombX];
+    bomb_tile.tile_value = 9;
 
     for(int i = -1; i <= 1; i++) {
         for(int j = -1; j <= 1; j++) {
             if((bombX + i >= 0) && (bombX + i < BOARD_SIZE)
             && (bombY + j >= 0) && (bombX + j < BOARD_SIZE)) {
-                Tile& updateTile = board[bombY + j][bombX + i];
-                if(updateTile.tileValue != 9) {
-                    updateTile.tileValue += 1;
+                Tile& update_tile = board[bombY + j][bombX + i];
+                if(update_tile.tile_value != 9) {
+                    update_tile.tile_value += 1;
                 }
             }
         }
     }
 }
 
-Tile** generateBombs(Tile** board, int bombs, int initialX, int initialY) {
+Board generate_game_board(Tile** board_tiles, int bombs, int initialX, int initialY) {
     int bombCount = bombs;
-    Tile** bombBoard = board;
+    Tile** game_board_tiles = board_tiles;
     int minSafeX = initialX == 0 ? 0 : initialX - 1;
     int maxSafeX = initialX == 9 ? 9 : initialX + 1;
     int minSafeY = initialY == 0 ? 0 : initialY - 1;
@@ -61,22 +49,22 @@ Tile** generateBombs(Tile** board, int bombs, int initialX, int initialY) {
         int canaditeY = rand() % BOARD_SIZE;
         int canaditeX = rand() % BOARD_SIZE;
 
-        Tile canaditeTile = board[canaditeY][canaditeX];
-        if (canaditeTile.tileValue != 9 && 
+        Tile canaditeTile = game_board_tiles[canaditeY][canaditeX];
+        if (canaditeTile.tile_value != 9 && 
             (canaditeX < minSafeX || canaditeX > maxSafeX || 
             canaditeY < minSafeY || canaditeY > maxSafeY)) {
-                markTiles(bombBoard, canaditeX, canaditeY);
+                mark_tiles(game_board_tiles, canaditeX, canaditeY);
                 bombCount -= 1;
         }
     }
 
-    return bombBoard;
+    return game_board;
 }
 
 int main() {
     srand(time(nullptr)); //Seed random so board is actually random
-    Tile** board = generateBlankBoard();
-    Tile** bombBoard = generateBombs(board, 10, 9, 9);
-    printBoard(bombBoard);
+    Board game_board = generate_game_board(generate_blank_board(), 10, 9, 9);
+    print_board(game_board);
     return 0;
 }
+*/
