925a0f778b2d08f0afe97ea22b2be5f1a57336bc
3yassu <68251430+3yassu@users.noreply.github.com>
2026-09-12 13:20:14 -0500

    remove max

--- src/board.cpp
+++ src/board.cpp
@@ -6,10 +6,6 @@ constexpr std::uint8_t min(std::uint8_t a, std::uint8_t b) {
 	return a < b ? a : b;
 }
 
-constexpr std::uint8_t max(std::uint8_t a, std::uint8_t b) {
-	return a > b ? a : b;
-}
-
 constexpr bool in_3_by_3_range(std::uint8_t x, std::uint8_t y, std::uint8_t new_x, std::uint8_t new_y) {
 	bool below = new_y <= min(y + 1, board_size), above = new_y >= min(y - 1, y);
 	bool left = new_x <= min(x + 1, board_size), right = new_x >= min(x - 1, x);
@@ -17,15 +13,14 @@ constexpr bool in_3_by_3_range(std::uint8_t x, std::uint8_t y, std::uint8_t new_
 }
 
 
-
-// Board::Board(std::size_t board_size)
-// Creates uninstantiated board
-
-
+// Board::generate_tile_values
+// Used by Board::Iniitialize
+// Adds the +1 to all surrounding non-bomb tiles
 void Board::generate_tile_values(std::uint8_t x, std::uint8_t y) {
+	// uint8_t is unsigned so if x/y = 0 x-1 will be bigger than x :)
 	std::uint8_t i = min(x - 1, x), j = min(y - 1, y);
 	constexpr std::uint8_t max_value = board_size - 1;
-	std::uint8_t fin_i = max(max_value, x + 1), fin_j = max(max_value, y + 1);
+	std::uint8_t fin_i = min(max_value, x + 1), fin_j = min(max_value, y + 1);
 	for(;i <= fin_i; i++) {
 		for(;j <= fin_j;j++) {
 			if(i == x && j == y)
@@ -36,8 +31,8 @@ void Board::generate_tile_values(std::uint8_t x, std::uint8_t y) {
 	}
 }
 
-
-
+// Board::initialize
+// Places bombs randomly 
 void Board::initialize(std::uint8_t nbombs, std::uint8_t x, std::uint8_t y) {
 	while(nbombs > 0) {
 		std::uint8_t rand_x = rand() % board_size;
